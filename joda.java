c8c8ae2aef4ae7fb7d7742837d33691d86bc2178
Patrik Kis <43583841+Pattesz1998@users.noreply.github.com>
2020-10-01 18:08:26 +0200

    Joda.Java file hozzáadása a repositoryhoz

---
+++ joda.java
@@ -0,0 +1,15 @@
+public class Yoda
+{
+public static void main (String[]args)
+{
+String myString = null;
+if (myString.equals("proba"))
+{
+System.out.println("egyenlő a probaval");
+}
+/*if ("proba".equals(myString))
+{
+System.out.println("egyenlő a null értékkel");
+}*/
+}
+}
